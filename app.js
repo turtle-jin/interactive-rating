fcc00aa6adda727e28654a04dccb5f85bbea594d
turtle <codingjinjin@gmail.com>
2024-02-29 15:57:24 -0600

    add button background color when clicked

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 
-const options = document.querySelectorAll(".options");
+const btns = document.querySelectorAll(".roundBtns");
+const submitBtn = document.querySelector(".submitBtn button");
 let displayResult = document.querySelector(".displayResult p")
-displayResult.textContent = "`You selected ${n} out of 5`";
+
+let submited = null;
+let btnSelected = null;
+btns.forEach(btn => {
+    btn.addEventListener("click", function(e) {
+        if (btnSelected) {
+            btnSelected.style.backgroundColor = "";
+        }
+
+        e.target.style.backgroundColor = "lightgrey";
+        btnSelected = e.target;
+        let n = e.target.textContent;
+
+        
+        
+       
+    });
+});
+
+displayResult.textContent = `You selected ${n} out of 5`;
+
+
+
