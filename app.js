d9d2413cb99d9e1b422f02f78164beaba90fdeef
turtle <codingjinjin@gmail.com>
2024-03-01 15:01:13 -0600

    complete

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 
 const btns = document.querySelectorAll(".roundBtns");
 const submitBtn = document.querySelector(".submitBtn button");
-let displayResult = document.querySelector(".displayResult p")
+let displayResult = document.querySelector(".displayResult p");
+const thankyouPage = document.querySelectorAll(".thankyouState");
+const ratingPage = document.querySelectorAll(".ratingState");
 
 let submited = null;
 let btnSelected = null;
+
 btns.forEach(btn => {
     btn.addEventListener("click", function(e) {
         if (btnSelected) {
             btnSelected.style.backgroundColor = "";
         }
-
         e.target.style.backgroundColor = "lightgrey";
         btnSelected = e.target;
         let n = e.target.textContent;
-
-        // need to add event listener of the summit btn
-        displayResult.textContent = `You selected ${n} out of 5`;
-       
+        displayResult.textContent = `You selected ${n} out of 5`;   
     });
 });
+
+submitBtn.addEventListener("click", () => {
+    ratingPage.forEach(element => {
+        element.style.display = "none";
+    })
+    thankyouPage.forEach(element => {
+        element.style.display = "flex";
+    });
+})
 
 
 
